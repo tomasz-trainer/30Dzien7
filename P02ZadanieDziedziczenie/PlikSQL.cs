694ecd0a654c5b776303f1d0a9d06eba38acf754
tomasz <csharp@406-00>
2026-07-07 08:56:59 +0200

    Add Raport base class and file-specific report types
    Implemented Raport base class with properties for report metadata. Added PlikCSV, PlikHTML, and PlikSQL classes inheriting from Raport, each with a Zapisz() method for saving content in their respective formats. Updated Program.cs to create and save reports using these new classes. Updated project file to include new source files.

---
+++ P02ZadanieDziedziczenie/PlikSQL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02ZadanieDziedziczenie
+{
+    internal class PlikSQL : Raport
+    {
+        public PlikSQL()
+        {
+            rozszerzenie = ".sql";
+        }
+
+        public void Zapisz()
+        {
+            Zawartosc = $"SELECT {Zawartosc} from Dane";
+            File.WriteAllText(Sciezka + "\\" + Nazwa + rozszerzenie, Zawartosc);
+        }
+    }
+}
