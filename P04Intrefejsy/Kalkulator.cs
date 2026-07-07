dce98a149a8930cbcac5d2dd893da41964e2c056
tomasz <csharp@406-00>
2026-07-07 10:07:38 +0200

    Add P04Intrefejsy project with calculator interfaces
    Added new P04Intrefejsy project to the solution. Implemented IOperacja interface, Kalkulator class, addition and subtraction mechanisms, and supporting files. Updated solution file to include the new project.

---
+++ P04Intrefejsy/Kalkulator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04Intrefejsy
+{
+    internal class Kalkulator
+    {
+        public double WykonajOperacje(int a, int b, IOperacja operacja)
+        {
+            double wynik = operacja.WykonajOperacje(a, b);
+            return wynik;
+        }
+
+
+    }
+}
---
+++ P04Intrefejsy/MechanizmOdejomowania.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04Intrefejsy
+{
+    internal class MechanizmOdejomowania : IOperacja
+    {
+        public double WykonajOperacje(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
